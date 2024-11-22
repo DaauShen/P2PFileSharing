71508b480945aa73fffb9adb3cb52afc38509379
DaauShen <93089549+DaauShen@users.noreply.github.com>
2024-11-23 00:04:33 +0700

    Update Downloader.py

--- Client/Downloader.py
+++ Client/Downloader.py
@@ -1,4 +1,4 @@
-import hashlib, math, os, socket, threading
+import base64, json, hashlib, os, socket, threading, time
 from zeroconf import Zeroconf
 
 config_file = "Config//client_config.txt"
@@ -48,121 +48,83 @@ class Downloader:
             print(f"[Error occured at start_downloader()]\n{e}")
 
     def download(self, file):
-        base, ext = file.split('.')
-        torrent_path = f"Torrents//{file}//{base}_{ext}.torrent"
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.server_ip,self.server_port)) #Connect to tracker
-            s.sendall("DOWNLOAD".encode())
-            response = s.recv(1024).decode()
-            if response == "NOTFOUND":
-                print(f"File {file} is not uploaded!")
-                return
-            elif response == "FOUND":
-                s.sendall("OK".encode())
-                with open(torrent_path, "wb") as writing_torrent:
-                    while chunk := s.recv(1024):
-                        writing_torrent.write(chunk)
-                s.sendall("OK".encode())
-            else: #Invalid data
-                return
-            online_list = []
-            while True:
-                data = s.recv(1024)
-                if data == "END":
-                    break
-                ip, port = data.split(' ')
-                try:
-                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as test_connection:
-                        test_connection.connect((ip,int(port)))
-                        test_connection.sendall("TEST")
-                        online_list.append((ip,port))
-                except Exception:
-                    pass
-                s.sendall("OK".encode())
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((self.server_ip, self.server_port))
+                s.sendall("DOWNLOAD".encode('utf-8'))
+                requirements = {"file": file, "downloader": (self.ip, self.port)}
+                time.sleep(0.5)
+                s.sendall(json.dumps(requirements).encode('utf-8'))
+                response = s.recv(1024).decode('utf-8')
+                if response == "NOTFOUND":
+                    print(f"File {file} not found.")
+                    return
+                data = s.recv(1024).decode('utf-8')
+                data = json.loads(data) 
+            os.makedirs(f"Torrents//{file}", exist_ok = True)
+            seeder_list = data["list"]
+            magnetinfo = data["magnetinfo"]
+            fragments = magnetinfo["fragments"]
+            assign_task = {}
+            for fragment in fragments:
+                for i in range(len(seeder_list)):
+                    ip, port = seeder_list[i]
+                    port = int(port)
+                    try:
+                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                            s.connect((ip, port))
+                            s.sendall("CHECK".encode('utf-8'))
+                            time.sleep(0.5)
+                            s.sendall(f"{file} {fragment}".encode('utf-8'))
+                            response = s.recv(1024).decode('utf-8')
+                            if response == "EXIST":
+                                assign_task[fragment] = (ip, port)
+                                break
+                            else:
+                                continue
+                    except Exception as e:
+                        continue
             
-        #Download each fragment
-        checksum_dict = {}
-        tasks = {}
-        parts = []
-        with open(torrent_path, "rb") as reading_torrent:
-            file_name, total_parts = reading_torrent.readline().decode().strip().split()
-            # Loop through the remaining lines
-            for line in reading_torrent:
-                # Split each line into part name and checksum
-                part_name, checksum = line.decode().strip().split()
-                parts.append(part_name)
-                checksum_dict[part_name] = checksum
-        for part in parts:
-            for i in range(len(online_list)):
-                ip, port = online_list[i]
-                try:
-                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                        s.connect((ip,int(port)))
-                        s.sendall("CHECK".encode())
-                        r = s.recv(1024).decode()
-                        s.sendall(f"{file} {part}".encode())
-                        r = s.recv(1024).decode()
-                        if r == "FOUND":
-                            tasks[part] = (ip, port)
-                            online_list = online_list[i+1:] + online_list[:i+1]
-                        else:
-                            continue
-                except Exception:
-                    continue
-        if len(parts) != total_parts:
-            print(f"File {file} is missing part(s)!")
-            return 
-        #After assign all tasks, start downloading by using multithread
-        check = {}
-        for part in tasks:
-            ip, port = tasks[part]
-            thr = threading.Thread(target = self.download_part, args = [file,part,ip,port,check])
-            thr.start()
+            thrs = []
+            for task in assign_task:
+                thr = threading.Thread(target = self.download_fragment, args = [file, task, assign_task[task]])
 
-        while len(check) != total_parts: #Waiting for all parts
-            pass
-
-        for part in check:
-            if check[part] != checksum_dict[part]:
-                print(f"File {file} is damaged!")
-                return
-        
-        with open(f"Downloads//{file}", "wb") as writing_file:
-            for part in parts:
-                with open(f"Torrents//{file}//{part}", "rb") as reading_part:
-                    while chunk := reading_part.read(1024):
-                        if not chunk:
-                            break
-                        writing_file.write(chunk)
-
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.server_ip,self.server_port))
-            s.sendall("ASSIGN".encode())
-            ok = s.recv(1024).decode()
-            s.sendall(f"{file} {self.ip} {self.port}".encode())
-
-
-    def download_part(self,file,part,ip,port,check):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((ip,int(port)))
-            s.sendall("REQUEST".encode())
-            ok = s.recv(1024).decode()
-            s.sendall(f"{file} {part}".encode())
-            with open(f"Torrents//{file}//{part}", "wb") as writing_part:
-                while chunk := s.recv(1024):
-                    if not chunk:
-                        break
-                    writing_part.write(chunk)
-        
-        check[file] = self.calculate_checksum(f"Torrents//{file}//{part}")
+            for thr in thrs:
+                thr.start()
             
-            
-                        
+            for thr in thrs:
+                thr.join()
 
-
+            with open(f"Downloads//{file}", "wb") as writing_file:
+                for fragment in fragments:
+                    with open(f"Torrents//{file}//{fragment}", "r") as reading_part:
+                        data = json.load(reading_part)
+                        writing_file.write(base64.b64decode(data["text"]))
                 
+            if self.calculate_checksum(f"Downloads//{file}") == magnetinfo["checksum"]:
+                print(f"[{file}] is downloaded successfully.")
+            else:
+                print(f"[{file}] is corrupted.")
 
-
+        except Exception as e:
+            print(f"[Error occured at download()]\n{e}")
+            
+    def download_fragment(self, file, fragment, seeder):
+        try:
+            ip, port = seeder
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((ip, port))
+                s.sendall("DOWNLOAD".encode('utf-8'))
+                time.sleep(0.5)
+                s.sendall(f"{file} {fragment}".encode('utf-8'))
+                data = s.recv(685*1024).decode('utf-8')
+            data = json.loads(data)
+            with open(f"Torrents//{file}//{fragment}", "w") as writing_part:
+                json.dump(data, writing_part, indent=4)
+            print(f"[{file}] is downloading: {fragment}")
+                
+        except Exception as e:
+            print(f"[Error occured at download_fragment()]\n{e}")        
 
 if __name__ == "__main__":
     downloader = Downloader()
