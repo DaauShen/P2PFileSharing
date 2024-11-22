7b8ff7fe9bbdcbb059022e27cd6ef2a6b618922b
DaauShen <93089549+DaauShen@users.noreply.github.com>
2024-11-23 00:04:52 +0700

    Update Uploader.py

--- Client/Uploader.py
+++ Client/Uploader.py
@@ -1,6 +1,5 @@
-import hashlib, math, os, socket, threading
+import base64, json, hashlib, math, os, socket, threading, time
 from zeroconf import Zeroconf
-
 
 config_file = "Config//client_config.txt"
 
@@ -50,40 +49,41 @@ class Uploader:
             print(f"[Error occured at start_uploader()]\n{e}")
 
     def upload(self, file):
-        if os.path.exists(f"Uploads//{file}"):
-            try:
-                os.makedirs(f"Torrents//{file}", exist_ok = False)
-                num = math.ceil(os.path.getsize(f"Uploads//{file}") / (512*1024))
-                base, ext = file.split('.')
-                with open(f"Uploads//{file}", "rb") as reading_file, open(f"Torrents//{file}//{base}_{ext}.torrent", "wb") as writing_torrent:
-                    writing_torrent.write(f"{file} {num}\n".encode())
-                    part = 1
-                    while True:
-                        chunk = reading_file.read(512*1024)
-                        if not chunk:
-                            break
-                        with open(f"Torrents//{file}//{base}_{ext}_part{part}.fragment", "wb") as writing_part:
-                            writing_part.write(chunk)
-                        writing_torrent.write(f"{base}_{ext}_part{part}.fragment {self.calculate_checksum(f"Torrents//{file}//{base}_{ext}_part{part}.fragment")}\n".encode())
-                        print(f"[{file}] is uploading: {round(part/num*100,2)} %")
-                        part += 1
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    s.connect((self.server_ip,self.server_port))
-                    s.sendall("UPLOAD".encode())
-                    temp = s.recv(1024).decode() #waiting
-                    s.sendall((f"{file} {self.ip} {self.port}").encode())
-                    temp = s.recv(1024).decode() #waiting
-                    with open(f"Torrents//{file}//{base}_{ext}.torrent", "rb") as reading_torrent:
-                        while chunk := reading_torrent.read(1024):
-                            if not chunk:
-                                break
-                            s.sendall(chunk)
-                    print(f"[{file}] is uploaded sucessfully!")
-            except FileExistsError:
-                print(f"File {file} is already uploaded!")
+        try:
+            if not os.path.exists(f"Uploads//{file}"):
+                print(f"File {file} not found.")
+                return
+            base, ext = file.split('.')
+            os.makedirs(f"Torrents//{file}", exist_ok = True)
 
-        else:
-            print(f"File {file} doesn't exist.")
+            totalparts = math.ceil(os.path.getsize(f"Uploads//{file}")/(512*1024))
+            magnetinfo = {}
+            with open(f"Uploads//{file}", "rb") as reading_file:
+                magnetinfo["file"] = file
+                magnetinfo["totalparts"] = totalparts
+                magnetinfo["checksum"] = self.calculate_checksum(f"Uploads//{file}")
+                magnetinfo["uploader"] = (self.ip, self.port)
+                magnetinfo["fragments"] = []
+
+                for i in range(totalparts):
+                    with open(f"Torrents//{file}//{base}_{ext}_{i+1}.fragment", "w") as writing_part:
+                        part = {}
+                        part["text"] = base64.b64encode(reading_file.read(512*1024)).decode('utf-8')
+                        json.dump(part, writing_part, indent=4)
+                        magnetinfo["fragments"].append(f"{base}_{ext}_{i+1}.fragment")
+                        print(f"[{file}] is uploading: {round((i+1)/totalparts*100,2)} %")
+                
+                with open(f"Torrents//{file}//{base}_{ext}.torrent", "w") as writing_magnetinfo:
+                    json.dump(magnetinfo, writing_magnetinfo, indent=4)
+            
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((self.server_ip,self.server_port))
+                s.sendall("UPLOAD".encode('utf-8'))
+                time.sleep(0.5)
+                s.sendall(json.dumps(magnetinfo).encode('utf-8'))
+
+        except Exception as e:
+            print(f"[Error occured at upload()]\n{e}")
 
 
 if __name__ == "__main__":
